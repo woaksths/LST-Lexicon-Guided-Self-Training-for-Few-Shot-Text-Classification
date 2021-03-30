fb735e63f08d67a3247dfc124a3529d2a313d6ca
jaeman <woaksths@gmail.com>
2021-03-30 22:03:43 +0900

    update augment: set temeperature hyperparam and split paragraph into sents as model are trained with sentences.

--- util/augment.py
+++ util/augment.py
@@ -23,13 +23,17 @@ stop_words = set(stopwords.words('english'))
 
 
 def back_translate_ru(text):
-    ru = en2ru.translate(text)
-    return ru2en.translate(ru)
+    hyp_list = [0.7, 0.8, 0.9]
+    hyp = random.choice(hyp_list)
+    ru = en2ru.translate(text, sampling=True, temperature=hyp)
+    return ru2en.translate(ru, sampling=True, temperature=hyp)
 
 
 def back_translate_de(text):
-    de = en2de.translate(text)
-    return de2en.translate(de)
+    hyp_list = [ 0.7, 0.8, 0.9]
+    hyp = random.choice(hyp_list)
+    de = en2de.translate(text, sampling=True, temperature=hyp)
+    return de2en.translate(de, sampling=True, temperature=hyp)
 
 
 def back_translate(texts, labels):
@@ -41,6 +45,35 @@ def back_translate(texts, labels):
         augmented_texts.append(back_translate_de(text))
         augmented_labels.append(label)
     return augmented_texts, augmented_labels 
+
+
+def back_translate_random_sampling():
+    pass
+
+
+def aug_backtranslate(text_list, targets):
+    print('aug_backtranslate')
+    augmented_texts = []
+    augmented_labels = []
+    
+    for text, label in zip(text_list, targets):
+        print('origin text', text, label)
+        sents = paragraph_to_sentences(text)
+        label = label.item()
+        
+        back_tr_sents = []
+        for sent in sents:
+            back_tr_sent = back_translate_ru(sent)
+            back_tr_sents.append(back_tr_sent)
+        
+        back_tr_sents = sentences_to_pargraph(back_tr_sents)
+        print('back_tr_sents', back_tr_sents)
+        print()
+        augmented_texts.append(back_tr_sents)
+        augmented_labels.append(label)
+    assert len(augmented_texts) == len(augmented_labels)
+    
+    return augmented_texts, augmented_labels
 
 
 def get_synonyms(word):
@@ -92,3 +125,51 @@ def word_replacement(texts, labels):
         replaced_labels.append(label)        
     return replaced_texts, replaced_labels
     
+    
+import nltk
+
+
+def sentences_to_pargraph(sents_list):
+    return ' '.join(sents_list)
+
+def paragraph_to_sentences(text):
+    sent_tokenizer = nltk.tokenize.sent_tokenize
+    text = text.strip()
+    sent_list  = sent_tokenizer(text)
+    has_long = False
+    for split_punc in [".", ";", ",", " ", ""]:
+        if split_punc == " " or not split_punc:             
+            offset = 100
+        else:
+            offset = 5
+        has_long = False
+        new_sent_list = []
+        for sent in sent_list :
+            if len(sent) < 300:
+                new_sent_list += [sent]
+            else:
+                has_long = True
+                sent_split = split_sent_by_punc(sent, split_punc, offset)
+                new_sent_list += sent_split
+        sent_list = new_sent_list
+        if not has_long :
+            break
+    return sent_list
+
+
+def split_sent_by_punc(sent, punc, offset):
+    """Further split sentences when nltk's sent_tokenizer fail."""
+    sent_list = []
+    start = 0
+    while start < len(sent):
+        if punc:
+            pos = sent.find(punc, start + offset)
+        else:
+            pos = start + offset
+        if pos != -1:
+            sent_list += [sent[start: pos + 1]]
+            start = pos + 1
+        else:
+            sent_list += [sent[start:]]
+            break
+    return sent_list
