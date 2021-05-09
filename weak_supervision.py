1d8162a5c941a36d891effed62e27869b660dc49
jaeman <woaksths@gmail.com>
2021-05-10 02:19:54 +0900

    clean up code:weak_supervision

--- weak_supervision.py
+++ weak_supervision.py
@@ -3,20 +3,37 @@ from nltk.corpus import opinion_lexicon
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 
+
 def get_lexicon():
+    # This lexicon must be used for sentiment classification (opinion).
+    # Opinion Lexicon (or Sentiment Lexicon) (Hu and Liu, KDD-2004).
     opinion_pos = opinion_lexicon.positive()
     opinion_neg = opinion_lexicon.negative()
-    lexicon = {0:opinion_neg, 1:opinion_pos}
+    lexicon = {0:opinion_neg, 1:opinion_pos} 
     return lexicon
+
 
 
 def guide_pseudo_labeling(pseudo_labeled, guide_type, lexicon=None):
     '''
     @param dataset type: dict(label:[tuple(text_id, decoded_text, pred_label, target, confidence)])
     '''
-    labels = pseudo_labeled.keys()
-    new_dataset = {label: [] for label in labels}
+    new_dataset = None
+    if guide_type == 'predefined_lexicon_pl':
+        lexicon = get_lexicon()
+        new_dataset = bow_PL(pseudo_labeled, lexicon)
+    elif guide_type == 'lexicon_pl':
+        new_dataset = lexicon_PL(pseudo_labeled, lexicon)
+    elif guide_type == 'weighted_lexicon_pl':
+        new_dataset = weighted_lexicon_PL(pseudo_labeled, lexicon)
+    return new_dataset
 
+
+
+def bow_PL(pseudo_labeled, lexicon):
+    labels = pseduo_labeled.keys()
+    new_dataset = {label:[] for label in labels}
+    
     for label in labels:
         for data in pseudo_labeled[label]:
             text_id = data[0]
@@ -24,93 +41,79 @@ def guide_pseudo_labeling(pseudo_labeled, guide_type, lexicon=None):
             model_pred = data[2]
             target = data[3]
             confidence = data[4]
-            guide_pred = None
             
-            if guide_type == 'predefined_lexicon':
-                # Conventional word count & predefined lexicon
-                lexicon = get_lexicon()
-                guide_pred = rule_base_with_lexicon1(lexicon, decoded_text)
-            elif guide_type == 'generated_lexicon':
-                # Conventional word count & generated lexicon
-                guide_pred = rule_base_with_lexicon2(lexicon, decoded_text)
-            elif guide_type == 'advanced_generated_lexicon':
-                pass
+            words = decoded_text.split(' ')
+            words = [lemmatizer.lemmatize(word) for word in words]
+            num_of_matching = {label:0 for label in labels}
             
-            if model_pred == guide_pred:
+            for word in words:
+                for index in labels:
+                    if word in lexicon[index]:
+                        num_of_matching[index] += 1
+            
+            lexicon_pred = -1
+            max_count = -1
+            is_tie = False
+            for index, count in num_of_matching.items():
+                if count > max_count:
+                    max_count = count
+                    lexicon_pred = index
+                    is_tie = False
+                elif count == max_count:
+                    is_tie = True
+            
+            if is_tie == True or max_count <2:
+                lexicon_pred = -1
+            if model_pred == lexicon_pred:
                 new_dataset[label].append((text_id, decoded_text, model_pred, target, confidence))
-                
     return new_dataset
 
 
-def rule_base_with_lexicon1(lexicon, text):
-    words = text.split(' ')
-    words = [lemmatizer.lemmatize(word) for word in words]
-    labels = lexicon.keys()
-    num_of_matching = {label:0 for label in labels}
+
+def lexicon_PL(pseudo_labeled, lexicon):
+    labels = pseudo_labeled.keys()
+    new_dataset = {label: [] for label in labels}
     
-    for word in words:
-        for label in labels:
-            if word in lexicon[label]:
-                num_of_matching[label] +=1
-    
-    predict_label = -1
-    max_count = -1
-    is_tie = False
-    for label, count in num_of_matching.items():
-        if count > max_count:
-            max_count = count
-            predict_label = label
+    for label in labels:
+        for data in pseudo_labeled[label]:
+            text_id = data[0]
+            decoded_text = data[1]
+            model_pred = data[2]
+            target = data[3]
+            confidence = data[4]
+            words = decoded_text.split(' ')
+            words = [lemmatizer.lemmatize(word) for word in words]
+            num_of_matching = {label:0 for label in labels}
+            
+            for word in words:
+                word_label = None
+                max_count = 0
+                is_tie = False
+                for index in list(lexicon.keys()):
+                    if word in lexicon[index]:
+                        count = lexicon[index][word]
+                        if max_count < count:
+                            max_count = count
+                            is_tie = False
+                            word_label = index
+                        elif max_count == count:
+                            is_tie = True
+                if is_tie is False and word_label is not None:
+                    num_of_matching[word_label] += 1 
+
+            lexicon_pred = -1 
+            max_count = 0
             is_tie = False
-        elif count == max_count:
-            is_tie = True
-    
-    if is_tie == True or max_count < 2: ## To do: set threshold -> matching count
-        predict_label = -1
-    return predict_label
-   
-    
-def rule_base_with_lexicon2(lexicon, text):
-    words = text.split(' ')
-    words = [lemmatizer.lemmatize(word) for word in words]
-    labels = list(lexicon.keys())
-    num_of_matching = {label:0 for label in labels}
-
-#     print('Lexicon : ', lexicon)
-#     print('Text : ', text)
-
-    for word in words:
-        word_label = None
-        max_count = 0
-        is_tie = False
-        for label in list(lexicon.keys()):
-            if word in lexicon[label]:
-#                 print('label {} word {} count {}'.format(label, word, lexicon[label][word]))
-                count = lexicon[label][word]
-                if max_count < count :
+            for index, count in num_of_matching.items():
+                if count > max_count:
                     max_count = count
+                    lexicon_pred = index
                     is_tie = False
-                    word_label = label
-                elif max_count == count:
+                elif count == max_count:
                     is_tie = True
-        if is_tie is False and word_label is not None:
-            num_of_matching[word_label] += 1
-    
-#     print('matching_res : ', num_of_matching)
-    pred_label = -1
-    max_count = 0 
-    is_tie = False
-    
-    for label, count in num_of_matching.items():
-        if count > max_count:
-            max_count = count
-            pred_label = label
-            is_tie = False
-        elif count == max_count:
-            is_tie = True
-    
-    if is_tie == True or max_count < 2:
-        pred_label = -1
-        
-#     print('pred_label', pred_label)
-#     print('\n\n')
-    return pred_label
+            if is_tie == True or max_count < 2:
+                lexicon_pred = -1
+                
+            if model_pred == lexicon_pred:
+                new_dataset[label].append((text_id, decoded_text, model_pred, target, confidence))
+    return new_dataset
